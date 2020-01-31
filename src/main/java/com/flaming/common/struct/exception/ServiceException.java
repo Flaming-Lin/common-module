4155397f060c7d3b725fdd6aed741e8d51c8b9a1
Flaming <120345427@qq.com>
2020-01-31 12:06:37 +0800

    update

---
+++ src/main/java/com/flaming/common/struct/exception/ServiceException.java
@@ -0,0 +1,28 @@
+package com.flaming.common.struct.exception;
+
+/**
+ * @Author: Flaming
+ * @Time: 2020-01-31 12:05
+ */
+public class ServiceException extends Exception {
+
+    public ServiceException() {
+        super();
+    }
+
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ServiceException(Throwable cause) {
+        super(cause);
+    }
+
+    protected ServiceException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
